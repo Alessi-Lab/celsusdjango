51edaef658e389101154c9894989ff07445e73d6
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-12-09 16:14:30 +0000

    Update settings.py

--- celsusdjango/settings.py
+++ celsusdjango/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL = ''
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_ROOT = os.environ.get("DJANGO_MEDIA_ROOT", BASE_DIR) / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
