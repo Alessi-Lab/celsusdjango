f3db90240f60ba43ec21c4c7e805c61ecebbbe43
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2023-01-17 17:55:40 +0000

    Update view_sets.py

--- celsus/view_sets.py
+++ celsus/view_sets.py
@@ -1059,7 +1059,7 @@ class CurtainViewSet(FiltersMixin, viewsets.ModelViewSet):
 class KinaseLibraryViewSet(FiltersMixin, viewsets.ModelViewSet):
     queryset = KinaseLibraryModel.objects.all()
     serializer_class = KinaseLibrarySerializer
-    permission_classes = [permissions.AllowAny,]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ("id", "entry")
     ordering = ("entry")
