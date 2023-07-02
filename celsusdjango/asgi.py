3fdfb5806520cc8e815a21f59ee3e72363f84213
Toan Phung <toan.phung@proteo.info>
2023-07-02 16:43:54 +0100

    update

--- celsusdjango/asgi.py
+++ celsusdjango/asgi.py
@@ -8,11 +8,23 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from celsusdjango.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celsusdjango.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
+    ),
 })
---
+++ celsusdjango/consumers.py
@@ -0,0 +1,33 @@
+import json
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class CurtainConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.session_id = self.scope['url_route']['kwargs']['session_id']
+        self.personal_id = self.scope['url_route']['kwargs']['personal_id']
+        await self.channel_layer.group_add(self.session_id, self.personal_id)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.session_id, self.personal_id)
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            self.session_id,
+            {
+                'type': 'chat_message',
+                'text': data['message']
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+
+
