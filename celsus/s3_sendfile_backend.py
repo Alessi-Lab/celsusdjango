a06e54702930f54c52f44854ee1233dae2d8c6a9
Toan Phung <toan.phung@proteo.info>
2023-07-31 11:53:43 +0100

    Update s3_sendfile_backend.py

--- celsus/s3_sendfile_backend.py
+++ celsus/s3_sendfile_backend.py
@@ -3,4 +3,5 @@ from rest_framework.response import Response
 
 def sendfile(request, filename, **kwargs):
     headers = {'Location': filename, 'Access-Control-Allow-Origin': request.headers['Origin']}
+    print(headers)
     return Response(status=status.HTTP_303_SEE_OTHER, headers=headers)
